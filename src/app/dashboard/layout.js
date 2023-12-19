fc292305fababf04bd53965d7547f525b1b46b2b
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-12-19 21:33:17 +0700

    Update Dashboard Layout

--- src/app/dashboard/layout.js
+++ src/app/dashboard/layout.js
@@ -3,7 +3,7 @@ import React from 'react';
 const DashboardLayout = () => {
     return (
         <div>
-            
+            This Layout is the nested Layout for Dashboard page. 
         </div>
     );
 };
