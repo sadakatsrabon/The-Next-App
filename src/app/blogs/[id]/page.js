77bc742e66bfa624f296a13ced13965b6844a225
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-12-20 11:32:32 +0700

    Setup

---
+++ src/app/blogs/[id]/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PrimaryBlog = () => {
+    return (
+        <div>
+            This Primary Blog page is for the testing purpose of Dynamig route.
+        </div>
+    );
+};
+
+export default PrimaryBlog;
---
+++ src/app/blogs/page.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Blog = () => {
+    return (
+        <div>
+            Thsi the mother route of dynmic route.
+        </div>
+    );
+};
+
+export default Blog;
