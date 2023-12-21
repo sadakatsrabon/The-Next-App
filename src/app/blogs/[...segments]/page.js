6282b41686372ebd3afaab3f5bfb65afa3ba966c
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-12-21 21:10:41 +0700

    Catch Dynamic routes

---
+++ src/app/blogs/[...segments]/page.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const PrimaryBlog = ({ params }) => {
+    const [year, id] = params.segments;
+    console.log(params.segments);
+    return (
+        <div className='p-6'>
+            <h2 className='text-center text-xl font-extrabold'>
+                This Primary Blog page is for the testing purpose of Dynamig route. {year}.
+            </h2>
+            <h4 className='text-center text-2xl'>Route number{id} from year: {year}</h4>
+        </div>
+    );
+};
+
+export default PrimaryBlog; 
