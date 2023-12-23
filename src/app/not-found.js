c4ab652f2782117f97c1967442aee03efb9520ed
Sadakat Hossain <sadakat.sh1@gmail.com>
2023-12-23 19:39:42 +0700

    Add a 404 Page

---
+++ src/app/not-found.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <div>
+                <h2 className='text-4xl mb-4 text-center mt-20 font-extrabold'>404</h2>
+                <p className='text-center text-red-500'>Page note found</p>
+            </div>
+        </div>
+    );
+};
+
+export default NotFound;
